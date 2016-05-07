9bd98fc80fdea5d42901dc1144d973a5399a2b02
Alfred Bratterud <alfred.bratterud@hioa.no>
2016-05-07 23:36:30 +0200

    Added URI-library dependency

--- inc/common.hpp
+++ inc/common.hpp
@@ -6,9 +6,9 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,13 +19,12 @@
 #define HTTP_COMMON_HPP
 
 #include <string>
+#include <uri>
 
 namespace http {
 
-using URI     = std::string;
-using Method  = std::string;
-
-using Limit   = std::size_t;
+  using URI     = uri::URI;
+  using Limit   = std::size_t;
 
 } //< namespace http
 
