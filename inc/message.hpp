2a5329c991cbc40824db039d8ab162c11f34c11e
Alfred Bratterud <alfred.bratterud@hioa.no>
2016-05-08 03:10:59 +0200

    inlined get_body

--- inc/message.hpp
+++ inc/message.hpp
@@ -6,9 +6,9 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -230,7 +230,7 @@ public:
   // @return - The object that invoked this method
   //----------------------------------------
   virtual Message& reset() noexcept;
-  
+
   //-----------------------------------
   // Get a string representation of this
   // class
@@ -327,7 +327,7 @@ inline Message& Message::add_body(Entity&& message_body) {
                     std::to_string(message_body_.size()));
 }
 
-const Message::Message_Body& Message::get_body() const noexcept {
+inline const Message::Message_Body& Message::get_body() const noexcept {
   return message_body_;
 }
 
