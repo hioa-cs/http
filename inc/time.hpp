b4078ce5f1d7380ca220b9beb697d51c814d1be1
RicoAntonioFelix <ricoantoniofelix@yahoo.com>
2016-05-25 12:47:51 -0400

    Added another time function...

--- inc/time.hpp
+++ inc/time.hpp
@@ -27,7 +27,7 @@ namespace time {
 // Get the time in {Internet Standard Format} from
 // a {time_t} object 
 //
-// @param time - The time_t object to get the time from
+// @param time_ - The time_t object to get the time from
 //
 // @return The current time as a std::string
 //
@@ -46,9 +46,43 @@ inline std::string from_time_t(const time_t& time_) {
 }
 
 //------------------------------------------------
+// Get a {time_t} object from a {std::string} representing
+// timestamps specified in RFC 2616 §3.3
+//
+// @param time_ - The {std::string} representing the timestamp
+//
+// @return A {time_t} object from {time_}
+//
+// @note Returns a default initialized {time_t} object if an error occurred
+//------------------------------------------------
+inline std::time_t to_time_t(const std::string& time_) {
+  std::tm tm{};
+
+  if (time_.empty()) goto error;
+
+  // Format: Sun, 06 Nov 1994 08:49:37 GMT
+  if (strptime(time_.c_str(), "%a, %d %b %Y %H:%M:%S %Z", &tm) not_eq nullptr) {
+    return std::mktime(&tm);
+  }
+
+  // Format: Sunday, 06-Nov-94 08:49:37 GMT
+  if (strptime(time_.c_str(), "%a, %d-%b-%y %H:%M:%S %Z", &tm) not_eq nullptr) {
+    return std::mktime(&tm);
+  }
+
+  // Format: Sun Nov  6 08:49:37 1994
+  if(strptime(time_.c_str(), "%a %b %d %H:%M:%S %Y", &tm) not_eq nullptr) {
+    return std::mktime(&tm);
+  }
+
+  error:
+  	return std::time_t{};
+}
+
+//------------------------------------------------
 // Get the current time in {Internet Standard Format}
 //
-// @return The current time as a std::string
+// @return The current time as a {std::string}
 //
 // @note Returns an empty string if an error occurred
 //------------------------------------------------
